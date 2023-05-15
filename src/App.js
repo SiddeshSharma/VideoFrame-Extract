558266d2763548ad6af02e287ee069dd8be86f69
Siddesh Sharma <siddesh@vigaet.com>
2023-05-15 22:05:02 +0530

    fixed error, added webgl renderer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,76 @@
-import './App.css';
-import { setUpFile } from './VideoUtility';
+import { useEffect, useRef } from "react";
+import "./App.css";
+import { getFrameByNumber2, setUpFile } from "./VideoUtility";
+import { WebGLRenderer } from "./Renderer";
 
 function App() {
+  const resquestedFrame = 100;
+  const canvasRef = useRef(null);
+  const renderer = useRef(null);
+  useEffect(() => {
+    if(canvasRef.current){
+      renderer.current = new WebGLRenderer("webgl", canvasRef.current);
+    }
+  
+    return () => {
+      
+    }
+  }, [])
+  
+  let frameTimestamp = 0;
+  const decoder = new window.VideoDecoder({
+    output: handleDecodedFrame,
+    error: (error) => {
+      console.log("some error", error);
+    },
+  });
 
-  function handleFileUpload(event){
+  function handleFileUpload(event) {
+    
     //check if the file uploaded is a valid h.264 codec video
     const file = event.target.files[0];
-    if(file){
-      setUpFile({videoSrc: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', srcType: 'url', onReadyCB: (handler) => {
-        console.log(handler);
-      }})
-
-
+    if (file) {
+      setUpFile({
+        videoSrc:
+          "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        srcType: "url",
+        onReadyCB: (config) => {
+          decoder.configure(config);
+        },
+        onEncodedChunk: (data) => {
+          console.log(data);
+          for (let i = 0; i < data.length; i++) {
+            if(data[i].number === resquestedFrame){
+              console.log(data[i].videoChunk);
+              frameTimestamp = data[i].videoChunk.timestamp;
+            }
+            if (data[i].videoChunk) {
+              decoder.decode(data[i].videoChunk);
+            }
+          }
+        },
+      });
     }
-
   }
 
-
+  function handleDecodedFrame(frame) {
+    console.log(frame, frameTimestamp);
+    if(frame.timestamp === frameTimestamp){
+      //render the image.
+      renderer.current.draw(frame);
+    }
+    //if frame is equal to selected frame then render the frame
+    frame.close();
+  }
 
   return (
     <div className="App">
-      Extract Frame from an Video.
-
-      Upload 
-
-      <input type="file" accept='video/mp4' onChange={handleFileUpload}/>
-
-      url 
-
-      <input type="url"/>
+      Extract Frame from an Video. Upload
+      <input type="file" accept="video/mp4" onChange={handleFileUpload} />
+      url
+      <input type="url" />
+      <button onClick={() => getFrameByNumber2(100)}>Start</button>
+      <canvas height={800} width={1200} ref={canvasRef}/>
     </div>
   );
 }
--- src/VideoUtility.js
+++ src/VideoUtility.js
@@ -1,21 +1,18 @@
 import mp4Box from "mp4box";
 let mp4File;
-let frameRate;
-let videoSrc;
+let frameRate = 24;
+let videoSrc = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
 let lastFrameNumber = 0;
+let handleEncodedChunk;
 
-const videoDecoder = new VideoDecoder({
-  output: (frame) => {
-    console.log(frame);
-    frame.close();
-  },
-  error: () => {}
-})
+//videoDecoder is avilable in secure context only!,
+//move this to app.js
 
-export function setUpFile({ videoSrc, srcType, onReadyCB }) {
+export function setUpFile({ videoSrc, srcType, onReadyCB, onEncodedChunk }) {
   videoSrc = videoSrc
   mp4File = mp4Box.createFile();
+  handleEncodedChunk = onEncodedChunk
 
   const handler = {
     pass: getFrameByNumber,
@@ -24,12 +21,12 @@ export function setUpFile({ videoSrc, srcType, onReadyCB }) {
   mp4File.onSamples= onSample;
 
   mp4File.onReady = (info) => {
+    console.log(info);
     //mdat box is parsed no video can be used
-    if (onReadyCB) onReadyCB(handler);
     const videoTrack = info.tracks.find((t) => "video" in t);
     mp4File.setExtractionOptions(videoTrack.id, null, {
       rapAlignment: true,
-      nbSamples: 10,
+      nbSamples: 100,
     });
     const config = {
       codec: videoTrack.codec,
@@ -37,9 +34,9 @@ export function setUpFile({ videoSrc, srcType, onReadyCB }) {
       codedHeight: videoTrack.track_height,
       codedWidth: videoTrack.track_width,
     };
-
-    VideoDecoder.configure(config);
-
+    
+    if (onReadyCB) onReadyCB(config);
+    
     frameRate = videoTrack.nb_samples / videoTrack.movie_timescale;
   
   };
@@ -50,11 +47,19 @@ export function setUpFile({ videoSrc, srcType, onReadyCB }) {
   }
 }
 
+export function getFrameByNumber2(frameNO){
+  getFrameByNumber(frameNO);
+}
+
 const gop = {};
+const resArray = [];
 let lastKey = 0;
+let abort = false;
 
 function onSample(id, user, samples, clipInfo){
   for(const sample of samples){
+    if(abort) return;
+    // console.log("rec samples", sample.number);
     //find closest key to frame requested
       const videoChunk = new window.EncodedVideoChunk({
         type: sample.is_sync ? "key" : "delta",
@@ -66,11 +71,17 @@ function onSample(id, user, samples, clipInfo){
       if(videoChunk.type === 'key'){
         lastKey = sample.number;
         gop[sample.number] = {frames: []};
+        gop[sample.number]?.frames.push( {videoChunk, number: sample.number} );
         
+      }else{
+        gop[sample.number]?.frames.push({videoChunk,number: sample.number});
       }
-      gop[sample.number]?.frames.push({videoChunk,number: sample.number});
-     if(sample.number === lastFrameNumber){
-      handleEncodedChunk(gop[lastFrameNumber]);
+      resArray.push({videoChunk, number: sample.number})
+     if(sample.number >= lastFrameNumber){
+      if(handleEncodedChunk){
+        handleEncodedChunk(resArray);
+        abort = true;
+      }
      } 
 
   }
@@ -81,29 +92,33 @@ function onSample(id, user, samples, clipInfo){
 
 function getFrameByNumber(frameNumber) {
   //seek
-  const buff = 10 * (1024 * 1021);
-  const time  =  frameNumber / frameRate;
-  const dataOffset = mp4File.seek(time, true)
+  const buff = 5 * (1024 * 1024);
+  const time  =  Number(frameNumber / frameRate).toFixed(2);
+  console.log("Seeking to TIme:", time);
+
+
+  const dataOffset2 = mp4File.seek(frameNumber + 100 / frameRate, true);
+
+  const dataOffset = mp4File.seek(time, true);
+
+  console.log(dataOffset);
   //fetch
-  fetchData(videoSrc, {start: dataOffset.offset, end: dataOffset.offset + buff}, (data) => {
-    mp4File.appendBuffer(data)
+  fetchData(videoSrc, {start: dataOffset.offset, end: dataOffset2.offset}, (data) => {
+    mp4File.appendBuffer(data);
+    console.log("calling start again");
+    mp4File.start();
   });
 
   lastFrameNumber = frameNumber;
 
   //read from decoder
-  mp4File.start();
   //return
 }
 
-function handleEncodedChunk({frames}){
-  for(const frame in frames){
-    videoDecoder.decode(frame)
-  }
 
-}
 
 async function fetchData(url, { start, end }, callback) {
+  console.log(Number(start).toFixed(),Number(end).toFixed());
   const response = await fetch(url, {
     headers: {
       Range: `bytes=${Number(start).toFixed()}-${Number(end).toFixed()}`,
